efbb246af4465ae230712e31084d5c24580cf8ae
Fangor <rfonger@gmail.com>
2016-04-02 17:13:05 -0600

    Adding Basic level design framework.

--- Assets/Resources/Scripts/SpawnObjects.cs
+++ Assets/Resources/Scripts/SpawnObjects.cs
@@ -5,10 +5,19 @@ public class SpawnObjects : MonoBehaviour {
 
 	public GameObject objectToSpawn;
 	public float spawnDelay = 1f;
+    public int enemySupply;
+    public float scrollSpeed;
+    private bool startSpawning = false;
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("Spawn", spawnDelay);
+        if (enemySupply > 0)
+        {
+            enemySupply--;
+            Invoke("Spawn", spawnDelay);
+        }
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = transform.up * scrollSpeed; //This is really down
 	}
 	
 	// Update is called once per frame
@@ -16,10 +25,27 @@ public class SpawnObjects : MonoBehaviour {
 	
 	}
 
-	private void Spawn()
-	{
-		Instantiate (objectToSpawn, transform.position, transform.rotation);
+    void LateUpdate()
+    {
+        if (transform.position.y < 5.16f)
+        {
+            transform.position = new Vector2(transform.position.x, 5.16f);
+            startSpawning = true;
+        }
+    }
 
-		Invoke ("Spawn", spawnDelay);
-	}
+    private void Spawn()
+	{
+        enemySupply--;
+		Instantiate (objectToSpawn, transform.position, transform.rotation);
+        if (enemySupply > 0 && startSpawning)
+        {
+            Invoke("Spawn", spawnDelay);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
 }

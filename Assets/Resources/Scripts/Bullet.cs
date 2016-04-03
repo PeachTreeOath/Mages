cdc39f6340198b95853947beb1d019de23a0f697
Fangor <rfonger@gmail.com>
2016-04-03 00:47:04 -0600

    Adding new weapons and fixing collisions.

--- Assets/Resources/Scripts/Bullet.cs
+++ Assets/Resources/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 	
 	public float speed;
     public bool isPassable; //Whether the bullet keeps going after it collides or not.
+    public int type;
 
 	// Use this for initialization
 	void Start ()
@@ -21,9 +22,7 @@ public class Bullet : MonoBehaviour
 
     void OnDestroy()
     {
-        Debug.Log("Bullet was destroyed.");
-        Debug.Log(StackTraceUtility.ExtractStackTrace());
-        //Debug.Break();
+
         
     }
 
@@ -35,14 +34,17 @@ public class Bullet : MonoBehaviour
 		case 0:
 			sprite.material = Resources.Load<Material> ("Materials/blueMat");
 			gameObject.layer = LayerMask.NameToLayer ("PlayerBullets");
+            type = 0;
 			break;
 		case 1:
 			sprite.material = Resources.Load<Material> ("Materials/redMat");
 			gameObject.layer = LayerMask.NameToLayer ("EnemyBullets");
+            type = 1;
 			break;
 		case 2:
 			sprite.material = Resources.Load<Material> ("Materials/yellowMat");
 			gameObject.layer = LayerMask.NameToLayer ("EnemyBullets");
+            type = 2;
 			break;
 		}
 	}
@@ -57,4 +59,34 @@ public class Bullet : MonoBehaviour
 		GetComponent<Rigidbody2D> ().velocity = transform.up * speed;
 	}
 
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        string name = col.gameObject.name;
+        Enemy enemy = col.gameObject.GetComponent<Enemy>();
+        Head playerHead = col.gameObject.GetComponent<Head>();
+
+        switch (type)
+        {
+            case 0:
+                //Friendly bullets
+                
+                if (enemy != null && !isPassable) 
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 1:
+            case 2:
+                //same case for 1 and 2;
+
+                //Enemy bullets or pvp bullets
+                if (playerHead != null)
+                {
+                    col.gameObject.GetComponent<Head>().Die();
+                }
+
+                break;
+        }
+
+    }
 }

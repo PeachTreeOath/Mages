84f57dd4922dcb13b19f4bc2201460c01f2c4b8f
Dave Flanagan <dflanagan314@gmail.com>
2016-04-03 02:16:29 -0400

    Added awesome Component for putting notes on GameObjects in the inspector

---
+++ Assets/Resources/Scripts/Note.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+//Part of the unity editor gameobject notes.  Do not delete!
+[AddComponentMenu("Note/Add Note")]
+public class Note : MonoBehaviour {
+    public string note = "(none)";
+}

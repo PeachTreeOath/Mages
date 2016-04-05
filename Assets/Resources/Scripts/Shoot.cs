daa5d7c7c81e6be02f745e384e724211d25e76e4
Fangor <rfonger@gmail.com>
2016-04-04 23:51:49 -0600

    Adding circle bullet pattern.

--- Assets/Resources/Scripts/Shoot.cs
+++ Assets/Resources/Scripts/Shoot.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Shoot : MonoBehaviour {
 
 	public GameObject bulletPrefab;
+    public GameObject tetherPrefab;
+    public float radiusFromTether;
 	public float speed;
 	public float shotDelay;
 	public int type;
-    public bool isPassable;
+    public bool isPassable = false;
 
 	private bool readyToShoot = true;
 
@@ -32,11 +34,33 @@ public class Shoot : MonoBehaviour {
 
 	private void CmdFire()
 	{
+        
 		Bullet bullet = ((GameObject)Instantiate (bulletPrefab, transform.position, transform.rotation)).GetComponent<Bullet>();
-		bullet.SetType (type);
-		bullet.SetSpeed (speed);
+        if (tetherPrefab != null)
+        {
+            RotatingTether tether = ((GameObject)Instantiate(tetherPrefab, transform.position, transform.rotation)).GetComponent<RotatingTether>();
+            //tether.objectToCircle = gameObject;
+            tether.transform.parent = transform;
+
+            bullet.transform.parent = tether.transform;
+            //Vector3 bulletPosition = tether.transform.position;
+            //bulletPosition.y = bulletPosition.y + radiusFromTether;
+            //bullet.transform.position = bulletPosition;
+            //bullet.transform.rotation = tether.transform.rotation;
+            //bullet.speed = 0;
+
+            //currentWeapon = (Weapon)Instantiate(nextWeapon, transform.position, transform.rotation);
+            //currentWeapon.transform.parent = this.transform;
+        }
+        else
+        {
+            bullet.SetSpeed(speed);
+        }
+
+        bullet.SetType (type);
         bullet.owner = GetComponentInParent<Player>();
         bullet.isPassable = isPassable;
+        
 		bullet.Fire ();
 	}
 }

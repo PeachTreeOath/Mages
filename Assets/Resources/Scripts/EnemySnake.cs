c5030a7bcc4435c8c984cf147d97920b92374954
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-05 21:51:21 -0600

    new enemy prefabs

---
+++ Assets/Resources/Scripts/EnemySnake.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySnake : MonoBehaviour {
+
+	public Vector2 topLeftBound;
+	public Vector2 bottomRightBound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

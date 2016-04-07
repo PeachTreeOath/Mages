47d31650b996ba7c6a5c4b4d0abe5679c6d6c9fa
Dave Flanagan <dflanagan314@gmail.com>
2016-04-06 20:53:13 -0400

    Figured out how to network.  Now I need to figure out how to csharp.  Everything was good up until the delegates showed up.

--- Assets/Resources/Scripts/GameNetMan.cs
+++ Assets/Resources/Scripts/GameNetMan.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
+using UnityEngine.EventSystems;
 
 public class GameNetMan : NetworkLobbyManager {
 
@@ -11,10 +12,14 @@ public class GameNetMan : NetworkLobbyManager {
     private GameObject roomListParent;
     [SerializeField]
     private GameObject roomTextPrefab;
+    [SerializeField]
+    private GameObject joinRoomInput;  //obviously view should be separated out here...but, uh, not my game
+    //[SerializeField]
+    //private GameObject joinRoomBut;
 
     private bool singlePlayer = false;
     private bool doRefresh = false;
-    private float refreshInterval = 5.0f;
+    private float refreshInterval = 2.0f;
     private float refreshTimeDue = 0;
 
 
@@ -72,26 +77,47 @@ public class GameNetMan : NetworkLobbyManager {
         }
         ListMatchRequest listReq = new ListMatchRequest();
         listReq.nameFilter = "";
-        listReq.pageNum = 1;
+        listReq.pageNum = 0;
         listReq.pageSize = 10;
         listReq.projectId = Application.cloudProjectId;
         matchMaker.ListMatches(listReq, roomListCallback);
-        //matchMaker.ListMatches(1, 10, "", roomListCallback);
     }
 
     public virtual void roomListCallback(ListMatchResponse resp) {
-        Debug.Log("List rooms was successful: " + resp.success);  //FIXME WHYYYYYYYYYYYYYYYYYYYYYYYYYYY doesn't this return any results ever?!?!!?!?!
-        Debug.Log("Found " + resp.matches.Count + " rooms");
-        //cleanup existing list
-        //foreach (Transform child in roomListParent.transform) {
-        //Destroy(child.gameObject);
-        //}
-        foreach (MatchDesc d in resp.matches) {
-            GameObject.Instantiate(roomTextPrefab);
-            roomTextPrefab.GetComponent<Text>().text = d.name;
-            roomTextPrefab.transform.SetParent(roomListParent.transform, false);
+        if(!doRefresh) {
+            return; //refreshes halted
         }
-        OnMatchList(resp);
+        Debug.Log("Found " + resp.matches.Count + " rooms");
+        matches = resp.matches;
+        //cleanup existing list
+        foreach (Transform child in roomListParent.transform) {
+        Destroy(child.gameObject);
+        }
+        foreach (MatchDesc d in resp.matches) {
+            GameObject go = GameObject.Instantiate(roomTextPrefab);
+            go.GetComponentInChildren<Text>().text = d.name + "\t(" + d.currentSize + "/" + d.maxSize + ")" ; //may be sub-sub component
+            go.transform.SetParent(roomListParent.transform, false); //Just parent the object to the panel and let the panel lay it out
+
+            //setup data for the selection obj
+            RoomData rd = go.AddComponent<RoomData>();
+            rd.setRec(joinRoomInput);
+            rd.setData(d);
+
+            //TODO: tie the selectable onclick to populate the text field with the server name. A SEEMINGLY IMPOSSIBLE TASK.
+            EventTrigger t = go.AddComponent<EventTrigger>();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Select;
+            entry.callback.AddListener((eventData) => { if (eventData.selectedObject == gameObject) {
+                    Debug.Log("Shit happened");
+                    } else {
+                    Debug.Log("Almost..." + gameObject.name);
+                    }
+                RoomData.handleSelection(eventData);
+            });
+            t.triggers.Add(entry);
+            
+        }
+        OnMatchList(resp); //maybe unnecessary
     }
 
     public void createRoom(GameObject text) {
@@ -99,7 +125,7 @@ public class GameNetMan : NetworkLobbyManager {
         if (roomName != null && roomName.Length > 0) {
             Debug.Log("CREATING SUPER SMALL ROOM BECAUSE WE NEED MORE CCUS");
             Debug.Log("Room name will be " + roomName);
-            //matchMaker.CreateMatch(roomName, 2, true, "", createRoomCallback);
+            //matchMaker.CreateMatch(roomName, 2, true, "", createRoomCallback); //we want moar control
             CreateMatchRequest matchReq = new CreateMatchRequest();
             matchReq.name = roomName;
             matchReq.size = 2;
@@ -113,15 +139,21 @@ public class GameNetMan : NetworkLobbyManager {
         Debug.Log("Room creation was successful: " + resp.success);
         if (resp.success) {
             //ideally we could continue to refresh and sit in the lobby here, but unity apparently will not allow us to create a game and not join it immediately.
+            doRefreshNow();
             doRefresh = false;
             OnMatchCreate(resp); //this will join immediately, which may be what we want once testing is done
-            Debug.Log("Joined");
             ServerChangeScene(playScene);  //is this necessary??
+            Debug.Log("Joined");
 
             Debug.Log("Scene switched");
         } else {
             doRefreshNow();
         }
+    }
+
+    public override void OnClientSceneChanged(NetworkConnection conn) {
+        Debug.Log("Intercepting lobby scene change handler");
+        //base.OnClientSceneChanged(conn);
     }
 
 
---
+++ Assets/Resources/Scripts/RoomData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RoomData : MonoBehaviour {
+
+    private GameObject joinGameInput;  //room name will be put here if selected from list
+
+    private MatchDesc listData;
+
+    public void setRec(GameObject inpTextField) {
+        joinGameInput = inpTextField;
+    }
+
+    public void setData(MatchDesc d) {
+        listData = d;
+        joinGameInput.GetComponent<Text>().text = d.name;
+    }
+
+    public delegate void selectionHandler(BaseEventData eventData);
+
+    public static selectionHandler handleSelection(BaseEventData d)  { Debug.Log("asdfasfas");return null; }
+}

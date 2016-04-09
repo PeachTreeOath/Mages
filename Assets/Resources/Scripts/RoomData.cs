3be79de0f39a992da76b4bb4ec193ed841583829
Dave Flanagan <dflanagan314@gmail.com>
2016-04-09 13:30:12 -0400

    Restoring this file that somehow Ryan's revert deleted

---
+++ Assets/Resources/Scripts/RoomData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class RoomData : MonoBehaviour, ISelectHandler {
+
+    private GameObject joinGameInput;  //room name will be put here if selected from list, type is InputField
+
+    private MatchDesc listData;
+
+    public void setReceiver(GameObject inpTextField) {
+        joinGameInput = inpTextField;
+    }
+
+    public void setData(MatchDesc d) {
+        listData = d;
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        if(listData != null) {
+            Debug.Log("Selected room " + listData.name);
+            Debug.Log("joinGame: " + joinGameInput.GetType().Name);
+            Debug.Log("joinGameparent: " + joinGameInput.transform.parent.name);
+            joinGameInput.GetComponentInChildren<InputField>().text= listData.name;
+        }
+    }
+}

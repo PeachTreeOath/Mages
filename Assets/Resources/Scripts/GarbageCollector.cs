76383c2cd4b20f6e90924b4b8cdf708117869977
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-10 22:21:50 -0600

    Replaced destroyoninvisible w manual boundary checking

---
+++ Assets/Resources/Scripts/GarbageCollector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarbageCollector : MonoBehaviour {
+
+	public Vector2 topLeftBounds;
+	public Vector2 bottomRightBounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("DeletePastBoundary");
+
+		foreach(GameObject obj in objs)
+		{
+			if (obj.transform.position.x < topLeftBounds.x || obj.transform.position.x > bottomRightBounds.x ||
+			   obj.transform.position.y > topLeftBounds.y || obj.transform.position.y < bottomRightBounds.y) {
+				Destroy (obj);
+			}
+		}
+	}
+}

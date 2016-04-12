b7ce290c18e110d2eb4d47aaecd45220bda54f9c
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-12 00:57:53 -0600

    Genie animation

---
+++ Assets/Resources/Scripts/AnimationSpeeder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSpeeder : MonoBehaviour {
+
+	public float animationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator> ().speed = animationSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

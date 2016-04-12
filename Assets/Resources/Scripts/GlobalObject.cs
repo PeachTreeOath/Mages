810528c99bd55992e32edcbc8b98f5ff09afecbc
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-11 23:00:26 -0600

    New scenes

---
+++ Assets/Resources/Scripts/GlobalObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalObject : MonoBehaviour {
+
+	public static GlobalObject instance;
+
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

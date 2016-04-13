07624bc3a49c8c432795da59067638d0df502d4b
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-12 23:50:07 -0600

    No more scene switch

--- Assets/Resources/Editor/EditorStartInLobby.cs
+++ Assets/Resources/Editor/EditorStartInLobby.cs
@@ -9,7 +9,7 @@ using System.Collections;
 //regardless of what scene is actually open in the editor.
 [InitializeOnLoad]
 public static class EditorStartInLobby {
-
+	
     [SerializeField]
     public static string startScene = "Offline";
 
@@ -26,7 +26,7 @@ public static class EditorStartInLobby {
                     //We're in playmode, right after having pressed Play
                     if(!SceneManager.GetActiveScene().name.Equals(startScene)){
                         Debug.Log("***EDITOR*** Switching to start scene");
-                        SceneManager.LoadScene(startScene);
+                        //SceneManager.LoadScene(startScene);
                     }
                     off = false;
                 } else {

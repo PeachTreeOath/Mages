78378052b15402b274558f39ae5e55d586355e60
PeachTreeOath <peachtreeoath@gmail.com>
2016-05-01 02:58:06 -0600

    Initial revive mechanic

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -17,8 +17,11 @@ public class Player : NetLifecycleObj
 	public float speed = 0;
 	public float timeToDie = 2.0f;
 	public List<Weapon> weaponLoadout = new List<Weapon> ();
+	public float reviveDistance = 1f;
 
 	private float deathStateTime;
+	private float reviveStateTime;
+
 	//used for several timings
 	private const float SPAWNING_TIME = 1.0f;
 	private const float UNCONSCIOUS_TIME = 2.0f;
@@ -30,11 +33,14 @@ public class Player : NetLifecycleObj
 	private Weapon nextWeapon;
 	private GameManager gameMgr;
 	private SpriteRenderer nextWeaponIcon;
+	private GameObject playerRevivingMe;
+	private ReviveSlot reviveSlot;
+	private Vector2 reviveActivatePosition;
 
 	void Start ()
 	{
-		gameMgr = GameObject.Find ("GameManager").GetComponent<GameManager>();
-		nextWeaponIcon = transform.Find ("WeaponIcon").GetComponent<SpriteRenderer>();
+		gameMgr = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+		nextWeaponIcon = transform.Find ("WeaponIcon").GetComponent<SpriteRenderer> ();
 		SpawnPlayer ();
 
 		int rand = UnityEngine.Random.Range (0, weaponLoadout.Count);
@@ -75,11 +81,13 @@ public class Player : NetLifecycleObj
 				transform.position = (Vector2)(transform.position) + new Vector2 (Input.GetAxis ("Horizontal_p" + playerNum) * Time.deltaTime * currSpeed, Input.GetAxis ("Vertical_p" + playerNum) * Time.deltaTime * currSpeed);
 			}
 			break;
-	
+		case PlayerState.REVIVING:
+			float reviveDelta = (Time.time - reviveStateTime) * 1f;
+			transform.position = Vector2.Lerp (reviveActivatePosition, reviveSlot.transform.position, reviveDelta);
+			break;
 		case PlayerState.DYING:
 			updateDeathState ();
 			break;
-
 		default:
 			Debug.Log ("Unhandled playerState " + playerState);
 			break;
@@ -142,7 +150,7 @@ public class Player : NetLifecycleObj
 		Invoke ("StartWeaponFlash", WEAPON_SWITCH_DELAY);
 	}
 
-	private void StartWeaponFlash()
+	private void StartWeaponFlash ()
 	{
 		StartCoroutine (FlashIcon (WEAPON_SWITCH_TIME, 0.05f));
 	}
@@ -159,11 +167,9 @@ public class Player : NetLifecycleObj
 	}
 
 	// Actually swaps weapons
-	private void CompleteSwitchWeapon()
+	private void CompleteSwitchWeapon ()
 	{
 		ToggleBarrels (currentWeapon, false);
-
-
 		ToggleBarrels (nextWeapon, true);
 		currentWeapon = nextWeapon;
 	}
@@ -182,7 +188,7 @@ public class Player : NetLifecycleObj
 
 	private void CmdServerRespawn ()
 	{
-		serverRespawn ();
+		Respawn ();
 	}
 
 	//This is called on collision trigger by the head
@@ -219,26 +225,86 @@ public class Player : NetLifecycleObj
 	//This code is executed on the server only
 	private void updateDeathState ()
 	{
-		//this needs work
+		bool actionPressed = false;
+		if (soloPlay) {
+			if (Input.GetButton ("Action_p" + playerNum + "_solo")) {
+				actionPressed = true;
+			}
+		} else {
+			if (playerNum < 4) {
+				// Players 1-4 in coop setting use dpad down to action
+				if (Input.GetAxisRaw ("Action_p" + playerNum) > 0) {
+					actionPressed = true;
+				}
+			} else {
+				// Players 5-8 are forced coop setting and use Y to action
+				if (Input.GetButton ("Action_p" + playerNum)) {
+					actionPressed = true;
+				}
+			}
+		}
+
+		if (actionPressed) {
+			GameObject closestPlayer = FindClosestPlayer ();
+			if (closestPlayer != null) {
+				reviveStateTime = Time.time;
+				playerState = PlayerState.REVIVING;
+				playerRevivingMe = closestPlayer;
+				reviveSlot = playerRevivingMe.GetComponent<Player> ().GetFreeReviveSlot (gameObject);
+				reviveActivatePosition = transform.position;
+				rend.enabled = false;
+				return;
+			}
+		}
+
 		float timeElapsed = Time.time - deathStateTime;
 		if (timeElapsed < timeToDie) {
 			transform.position = (Vector2)transform.position + (UnityEngine.Random.insideUnitCircle * timeElapsed * 0.02f);
 		} else {
 			GetComponent<GibManual> ().Explode ();
 			deathState = DeathState.FINISHED;
-			serverRespawn ();
+			Respawn ();
 		}
 	}
 
-	private void serverRespawn ()
+	private GameObject FindClosestPlayer ()
 	{
-		//still on the server
-		Debug.Log ("Server respawning player");
-		RpcRespawn ();
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Player");
+		GameObject closestObj = null;
+		float minDist = 100;
+		foreach (GameObject obj in objs) {
+			if (obj.GetInstanceID () == gameObject.GetInstanceID ()) {
+				continue;
+			}
+			float dist = Vector2.Distance (transform.position, obj.transform.position);
+			if (dist < reviveDistance) {
+				if (closestObj == null || dist < minDist) {
+					closestObj = obj;
+					minDist = dist;
+				}
+			}
+		}
+
+		return closestObj;
 	}
 
-	//[ClientRpc]
-	private void RpcRespawn ()
+	// Give a random revive slot to a player trying to revive
+	public ReviveSlot GetFreeReviveSlot (GameObject callingPlayer)
+	{
+		Transform slots = transform.Find ("ReviveSlots");
+		ReviveSlot[] reviveSlots = slots.gameObject.GetComponentsInChildren<ReviveSlot> ();
+		int slot = UnityEngine.Random.Range (0, reviveSlots.Length);
+		GameObject player = reviveSlots [slot].player;
+		while (player != null) {
+			slot = UnityEngine.Random.Range (0, reviveSlots.Length);
+			player = reviveSlots [slot].player;
+		}
+		// player has to be null at this point
+		reviveSlots [slot].player = callingPlayer;
+		return reviveSlots [slot];
+	}
+
+	private void Respawn ()
 	{
 		if (deathState == DeathState.FINISHED) {
 			deathState = DeathState.STARTING;

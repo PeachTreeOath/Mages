a56c88345d9ea02c90fc209f16d72b04887755d1
Fangor <rfonger@gmail.com>
2016-05-01 15:04:13 -0600

    Starting genie phase 3

---
+++ Assets/Resources/Scripts/BarrelRain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelRain : Barrel {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
